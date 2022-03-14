1c82d92a6b6833b49fa8c4c15425d9a74b49d5f7
Alan Rice <mail@alanrice.me>
2022-03-14 16:56:55 -0400

    params

--- backend/core/connection/slack.ts
+++ backend/core/connection/slack.ts
@@ -1,29 +1,11 @@
 import { InstallProvider } from "@slack/oauth";
-import AWS from "aws-sdk";
+import { Config } from "@serverless-stack/node/config";
 
 export async function auth_start(user: string) {
-  const ssm = await new AWS.SSM();
-  const SSM_NAMES = {
-    client_id: "/mangrove/dev/SLACK_CLIENT_ID",
-    client_secret: "/mangrove/dev/SLACK_CLIENT_SECRET",
-  };
-
-  const [client_id, client_secret] = await ssm
-    .getParameters({
-      Names: [SSM_NAMES.client_id, SSM_NAMES.client_secret],
-    })
-    .promise()
-    .then((resp) => {
-      return [
-        resp.Parameters?.find((p) => p.Name === SSM_NAMES.client_id)!.Value!,
-        resp.Parameters?.find((p) => p.Name === SSM_NAMES.client_secret)!
-          .Value!,
-      ];
-    });
-
+  console.log(Config);
   const installer = new InstallProvider({
-    clientId: client_id,
-    clientSecret: client_secret,
+    clientId: Config.SLACK_CLIENT_ID,
+    clientSecret: Config.SLACK_CLIENT_SECRET,
     stateSecret: JSON.stringify({ app: "mangrove" }),
   });
 
