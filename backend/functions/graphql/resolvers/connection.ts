d3a93c4626f1b2fde7accf51cfb11e874ecd518b
Alan Rice <mail@alanrice.me>
2022-03-15 18:34:44 -0400

    list conns resolver

--- backend/functions/graphql/resolvers/connection.ts
+++ backend/functions/graphql/resolvers/connection.ts
@@ -5,6 +5,9 @@ export const ConnectionResolver: Resolvers = {
   User: {
     connections: async (parent, args, _ctx) => {
       const plaid = await Connection.Plaid.connections(parent.id!);
+      const slack = await Connection.Slack.connections(parent.id!);
+
+      return [...plaid, ...slack];
     },
   },
 };
