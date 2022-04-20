3651863fbff4b74558726ca9b5bb94a389d7fe9f
Alan Rice <mail@alanrice.me>
2022-04-20 17:29:14 -0400

    Electrodb (#13)
    * electrodb
    
    * number filter
    
    * setup

--- backend/core/pipe/pipe.ts
+++ backend/core/pipe/pipe.ts
@@ -1,46 +1,246 @@
 import { Dynamo } from "@mangrove/core/dynamo";
-import { Entity } from "dynamodb-onetable";
+// import { Entity } from "dynamodb-onetable";
 import { Connection } from "@mangrove/core/connection";
 
-type Pipe = Entity<typeof Dynamo.Schema.models.Pipe>;
-type NumberFilter = Entity<typeof Dynamo.Schema.models.NumberFilter>;
-type StringFilter = Entity<typeof Dynamo.Schema.models.StringFilter>;
-type Source = Entity<typeof Dynamo.Schema.models.Source>;
-type Destination = Entity<typeof Dynamo.Schema.models.Destination>;
+import { Entity, EntityItem } from "electrodb";
+import { Pipe } from ".";
+import { Source } from "graphql";
+import { EventSourceState } from "@aws-sdk/client-eventbridge";
+import { FilterNode } from "kysely";
 
-const NumberFilter = Dynamo.Table.getModel<NumberFilter>("NumberFilter");
-const Pipe =
-  Dynamo.Table.getModel<Entity<typeof Dynamo.Schema.models.Pipe>>("Pipe");
-
-export async function list(user: string) {
-  const items = await Dynamo.Table.queryItems(
-    {
-      pk: `user#${user}`,
-      sk: { begins: "pipe#" },
+const PipeEntity = new Entity({
+  model: {
+    entity: "Pipe",
+    version: "1",
+    service: "mangrove",
+  },
+  attributes: {
+    pipeId: {
+      type: "string",
+      required: true,
+      readOnly: true,
     },
-    { parse: true }
-  );
+    userID: {
+      type: "string",
+      required: true,
+      readOnly: true,
+    },
+    name: {
+      type: "string",
+    },
+    flags: {
+      type: "map",
+      properties: {
+        enabled: {
+          type: "boolean",
+          required: true,
+        },
+      },
+    },
+  },
+  indexes: {
+    primary: {
+      pk: {
+        field: "pk",
+        composite: ["pipeID"],
+      },
+      sk: {
+        field: "sk",
+        composite: ["pipeID"],
+      },
+    },
+    user: {
+      index: "gs1",
+      pk: {
+        field: "gs1pk",
+        composite: ["userID"],
+      },
+      sk: {
+        field: "gs1sk",
+        composite: []
+      },
+    },
+  },
+});
 
-  const pipes = await Promise.all(
-    items
-      .filter((item) => item.type === "PIPE")
-      .map((item) => item as Pipe)
-      .map(async (pipe) => {
-        const sources = await build_sources(user, pipe.id, items);
-        const destinations = await build_destinations(user, pipe.id, items);
+const PlaidSourceEntity = new Entity({
+  model: {
+    entity: "PlaidSource",
+    version: "1",
+    service: "mangrove",
+  },
+  attributes: {
+    pipeID: {
+      type: "string",
+      required: true,
+      readOnly: true,
+    },
+    sourceID: {
+      type: "string",
+      required: true,
+      readOnly: true,
+    },
+    plaidItem: {
+      type: "string",
+      required: true,
+      readOnly: false,
+    },
+    plaidAccount: {
+      type: "string",
+      required: true,
+      readOnly: false,
+    },
+  },
+  indexes: {
+    primary: {
+      pk: {
+        field: "pk",
+        composite: ["pipeID"],
+      },
+      sk: {
+        field: "sk",
+        composite: ["sourceID"],
+      },
+    },
+  },
+});
 
-        return {
-          flags: {
-            enabled: pipe.flag_enabled,
-          },
-          sources,
-          destinations,
-        };
-      })
-  );
+const NumberFilterEntity = new Entity({
+  model: {
+    entity: "NumberFilter",
+    version: "1",
+    service: "mangrove",
+  },
+  attributes: {
+    filterID: {
+      type: "string",
+      required: true,
+      readOnly: true,
+    },
+    pipeID: {
+        type: "string", 
+        required: true, 
+        readOnly: true
+    }, 
+    sourceID: {
+      type: "string",
+      required: true,
+      readOnly: true,
+    },
+    op: {
+      type: "string",
+      required: true,
+      readOnly: false,
+    },
+    kind: {
+      type: "string",
+      required: true,
+      readOnly: true,
+    },
+    value: {
+      type: "number",
+      required: true,
+      readOnly: false,
+    },
+  },
+  indexes: {
+      numberFilter: {
+          pk: {
+              field: "pk", 
+              composite: ["filterID"], 
+          }, 
+          sk: {
+              field: "sk", 
+              composite: ["pipeID", "sourceID"]
+          } 
+      }, 
+      pipe: {
+          index: "gsi1", 
+          pk: {
+              field: "gsi1pk", 
+              composite: ["pipeID"]
+          }, 
+          sk: {
+              field: "gsi1sk", 
+              composite: ["sourceID"]
+          }
+      }, 
+      source: {
+          index: "gsi2", 
+          pk: {
+              field: "gsi2pk", 
+              composite: ["sourceID"]
+          }, 
+          sk: {
+              field: "gsi2sk", 
+              composite: []
+          }
+      }
+  },
+});
 
-  return pipes;
+type PipeEntityType = EntityItem<typeof PipeEntity>
+
+export async function for_user(user: string) {
+    const pipes = await PipeEntity.query.user({ userID: user }).go();
 }
+
+async function sources(pipe: string) {
+    const sources = await PlaidSourceEntity.query.primary({pipeID: pipe}).go()
+    
+    for (let source of sources) {
+        const filters = await NumberFilterEntity.query.source({ sourceID: source.sourceID})
+    }
+}
+
+export async function filters(pipe: string, source: string ) {
+    return NumberFilterEntity.query.pipe({pipeID: pipe, sourceID: source}).go()
+}
+
+
+
+//   get all pipes for a user 
+//  get all sources for a pipe 
+//  get all filters for source 
+
+//   const plaid_source_query = PlaidSourceEntity.query.pipes({ })
+  
+//   for (let pipe of pipes) {
+//       plaid_source_query.where({})
+//   }
+
+//   const sources = await PlaidSourceEntity.query.primary({pipeID => })
+}
+
+// export async function list(user: string) {
+//   const items = await Dynamo.Table.queryItems(
+//     {
+//       pk: `user#${user}`,
+//       sk: { begins: "pipe#" },
+//     },
+//     { parse: true }
+//   );
+
+//   const pipes = await Promise.all(
+//     items
+//       .filter((item) => item.type === "PIPE")
+//       .map((item) => item as Pipe)
+//       .map(async (pipe) => {
+//         const sources = await build_sources(user, pipe.id, items);
+//         const destinations = await build_destinations(user, pipe.id, items);
+
+//         return {
+//           flags: {
+//             enabled: pipe.flag_enabled,
+//           },
+//           sources,
+//           destinations,
+//         };
+//       })
+//   );
+
+//   return pipes;
+// }
 
 async function build_sources(
   user: string,
